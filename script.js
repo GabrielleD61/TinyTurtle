0a194d0e837d620840db72ff45caf676d914643e
Gabrielle Deleon <gdeleon.exol@gmail.com>
2017-01-19 21:42:47 +0000

    i made a house

---
+++ script.js
@@ -0,0 +1,15 @@
+TinyTurtle.apply(window); 
+
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(90);
+forward(40);
+right(40);
+forward(30);
+right(100);
+forward(29);
